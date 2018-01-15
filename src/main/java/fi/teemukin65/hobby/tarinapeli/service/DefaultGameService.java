607ec5ca94220e43fede7aaac735a0a9a5ec4eb4
teemukin65 <teemukin65@gmail.com>
2018-01-15 15:48:06 +0200

    Drafting of Game add and list services
    Signed-off-by: Teemu Kurki <teemu.kurki@gofore.com>

--- src/main/java/fi/teemukin65/hobby/tarinapeli/service/DefaultGameService.java
+++ src/main/java/fi/teemukin65/hobby/tarinapeli/service/DefaultGameService.java
@@ -1,28 +1,66 @@
 package fi.teemukin65.hobby.tarinapeli.service;
 
+import fi.teemukin65.hobby.tarinapeli.dao.GameRepository;
+import fi.teemukin65.hobby.tarinapeli.dao.PlayerRepository;
+import fi.teemukin65.hobby.tarinapeli.domain.Game;
+import fi.teemukin65.hobby.tarinapeli.domain.Player;
 import fi.teemukin65.hobby.tarinapeli.rest.dto.GameAddDto;
 import fi.teemukin65.hobby.tarinapeli.rest.dto.GameDto;
+import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.security.Principal;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class DefaultGameService implements GameService {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
+    private GameRepository gameRepository;
+
+    private PlayerRepository playerRepository;
+
+    private ModelMapper modelMapper;
+
+    DefaultGameService(
+            GameRepository gameRepository,
+            PlayerRepository playerRepository,
+            ModelMapper modelMapper) {
+        this.gameRepository = gameRepository;
+        this.playerRepository = playerRepository;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public List<GameDto> listUserGames(Principal user) {
         LOGGER.debug("getting list of games for the user:{} [{}]", user.getName(), user.toString());
 
-        return null;
+        // TODO: restrict games for games where logged in user is in
+        ArrayList<GameDto> gameDtos = new ArrayList<>();
+        Iterable<Game> games = gameRepository.findAll();
+        LOGGER.info("Found {} Games");
+        games.forEach((game) -> {
+            LOGGER.debug("listUserGames..found game:{}", game);
+            gameDtos.add(modelMapper.map(game, GameDto.class));
+        });
+        return gameDtos;
     }
 
     @Override
-    public GameDto addGame(GameAddDto newGame, Principal initiator) {
-        return null;
+    @Transactional
+    public GameDto addGame(GameAddDto gameInfo, Principal initiator) {
+        Game newGame = new Game();
+        newGame.setGameInitiationTime(new Timestamp(System.currentTimeMillis()));
+        newGame.setGameTitle(gameInfo.getGameTitle());
+        newGame.setGameDescription(gameInfo.getGameDescription().trim());
+        Player initiatingPlayer = playerRepository.findByEmail(initiator.getName().trim());
+        newGame.setGameInitiator(initiatingPlayer.getId());
+        return modelMapper.map(gameRepository.save(newGame), GameDto.class);
     }
 
     @Override
